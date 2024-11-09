20f9ed770e4108fbffc97e304c39505d1f04d79b
Yuxin Geng <yuxin.evol@gmail.com>
2024-11-09 12:08:56 +0800

    Add function truncate_colormap

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yuxin_helpers',
-    version='0.1.0',
+    version='0.1.5',
     packages=find_packages(),
     author='Yuxin Geng',
     author_email='yuxin.evol@gmail.com',
--- yuxin_helpers/__init__.py
+++ yuxin_helpers/__init__.py
@@ -1,4 +1,6 @@
 # yuxin_helpers/__init__.py
-from .color_utils import rgb_to_hex, hex_to_rgb
+from .color_utils import rgb_to_hex, hex_to_rgb, truncate_colormap
 
-__all__ = ['rgb_to_hex', 'hex_to_rgb']
+__version__ = "0.1.5"
+
+__all__ = ['rgb_to_hex', 'hex_to_rgb', 'truncate_colormap', '__version__']
